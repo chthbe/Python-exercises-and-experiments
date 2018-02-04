8c652ce16beaaf9d8348a5b68386893b677799a3
chthbe <chthbe@web.de>
2018-02-04 19:46:13 +0100

    Evi_spot_challenge_main.py changes:
    - Added correct cleaning of the test_data (no outcomes!)
    - Cleaned up old comments
    - Still missing: Cleaning of outcomes for training set
    - Still missing: Selecting training and validation sets

--- evispot challenge/Evispot_challenge_main.py
+++ evispot challenge/Evispot_challenge_main.py
@@ -16,7 +16,8 @@ from collections import Counter
 
 # Load the data from csv file. df is for data frame, a R and pandas class
 
-training_data = pd.read_csv('C:\\Users\\Becks\\Desktop\\training_test.csv')
+training_data = pd.read_csv('C:\\Users\\Becks\\Desktop\\training_test.csv', low_memory=False)
+test_data = pd.read_csv('C:\\Users\\Becks\\Desktop\\test_test.csv', low_memory=False)
 #training_data = pd.read_csv('C:\\Users\\Becks\\Desktop\\training_data.csv')
 #test_data = pd.read_csv('C:\\Users\\Becks\\Desktop\\test_data.csv')
 
@@ -29,26 +30,35 @@ training_data = pd.read_csv('C:\\Users\\Becks\\Desktop\\training_test.csv')
 
 # Keeping only data that is needed as input
 training_data = training_data [['BIRTH_YEAR', 'DATE', 'TRANS_AMO', 'MRCH_CITY', 'MRCH_CTRY', 'SEX', 'TRANSTYP_CODE']]
-#test_data = test_data [['BIRTH_YEAR', 'DATE', 'TRANS_AMO', 'MRCH_CITY', 'MRCH_CTRY', 'SEX', 'TRANSTYP_CODE']]
 
 # Create the clean input data as an empty list to which the arrays of data are added consequently
 c_training = []
 c_test = []
 
-# Create variable for the amount of rows of data
+# Create variable for the amount of rows of training data
 n_rows, ncols = training_data.shape
 print(n_rows, ncols)
 
 # For all columns, extract the data that is needed and then append it to the clean input list
 for i in training_data.keys():
 
-    # Convert column to list
-    temp = training_data[i].values
+    # Check whether feature is in both data sets, if not skip, else continue with loop execution for the feature
+    if i not in test_data.keys():
+        print('Error: Missing features in test data!')
+        continue
+    
+    else:
+        # Convert column to list    
+        temp = training_data[i].values
+        temp_test = test_data[i].values
 
     # If the data is numeric, just convert to numeric
     if i in ['BIRTH_YEAR','TRANS_AMO']:
-        temp = [s.replace(' ','NaN').replace(',','.') for s in temp]
+        temp = [str(s).replace(' ','NaN').replace(',','.') for s in temp]
         temp = np.array(temp).astype(np.float)
+        
+        temp_test = [str(s).replace(' ','NaN').replace(',','.') for s in temp_test]
+        temp_test = np.array(temp_test).astype(np.float)
 
         """
         # Replace missing data with the mean of the non-missing data, this will lead to 0s after normalising as it cannot change the mean
@@ -59,6 +69,7 @@ for i in training_data.keys():
     
         # Append data to the input data
         c_training.append(temp)
+        c_test.append(temp_test)
 
     # Now for the date split
     elif i in ['DATE']:
@@ -66,23 +77,35 @@ for i in training_data.keys():
         # First extract day of the month
         dom = np.array([int(s.split('/')[1]) for s in temp])
         c_training.append(dom)
+        
+        dom = np.array([int(s.split('/')[1]) for s in temp_test])
+        c_test.append(dom)
 
         # Now extract month
         month = np.array([int(s.split('/')[0]) for s in temp])
         c_training.append(month)
+        
+        month = np.array([int(s.split('/')[0]) for s in temp_test])
+        c_test.append(month)
 
         # Now get weekday (as integer 0 for monday to 6 for sunday)
         wd = np.array([dt.strptime(s, '%m/%d/%Y').weekday() for s in temp])
         c_training.append(wd)
+        
+        wd = np.array([dt.strptime(s, '%m/%d/%Y').weekday() for s in temp_test])
+        c_test.append(wd)
 
         # Finally convert the date to an epoch integer
         temp = np.array([dt.strptime(s, '%m/%d/%Y').timestamp() for s in temp])
         c_training.append(temp)
+        
+        temp_test = np.array([dt.strptime(s, '%m/%d/%Y').timestamp() for s in temp_test])
+        c_test.append(temp_test)
 
     # Next will be the sex and trans_code categories, where all are needed but empty categories will not be added
     elif i in ['SEX', 'TRANSTYP_CODE']:
         
-        # Get unique categories
+        # Get unique categories, taken only from training set as unknown categories from test set cannot be trained through the model
         unique_cat = np.unique(temp).tolist()
 
         # Drop empty categories
@@ -92,10 +115,11 @@ for i in training_data.keys():
         # For all unique categories, add a vector that is 1 if an event belongs to the category or 0 otherwise
         for uc in unique_cat:
             c_training.append(np.array([int(x == uc) for x in temp]))
+            c_test.append(np.array([int(x == uc) for x in temp_test]))
 
     # Finally will be the category columns that need to be split into multiple binary columns but cannot use every possible category
     else:
-        # Count how often each categoy occurs
+        # Count how often each categoy occurs, taken only from training set as unknown categories from test set cannot be trained through the model
         count = Counter(temp)
         # Select only the most important categories, in this case all on or above the 80th percentile
         imp_cat = [x for x,y in count.items() if y >= np.percentile(list(count.values()),80)]
@@ -103,34 +127,16 @@ for i in training_data.keys():
         # For all important categories, add a vector that is 1 if an event belongs to the category or 0 otherwise
         for ic in imp_cat:
             c_training.append(np.array([int(x == ic) for x in temp]))
+            c_test.append(np.array([int(x == ic) for x in temp_test]))
 
         # Summarise all other categories in one 'other' category
         c_training.append(np.array([int(x not in imp_cat) for x in temp]))
-print(c_training)
+        c_test.append(np.array([int(x not in imp_cat) for x in temp_test]))
+
+
+#print(c_training)
 
 """
-for col in ['MRCH_CITY', 'MRCH_CTRY', 'SEX', 'TRANSTYP_CODE']:
-    # Look at all strings in the training_data and pick most common
-    
-    temp = str(training_data[col])
-    print(type(temp))
-    temp = Counter(temp)
-    # Count the occurrence of the different categories in the string features and take only most common
-    print(temp)
-    
-    for new_col in temp.keys():
-    # For all categories add a new column and set it to 1 if the user belongs to the category or 0 otherwise
-    
-        c_training.loc [:,new_col] = np.where(training_data.loc[:,col] == new_col, 1, 0)
-        #c_test [new_col] = np.where(test_data[col] == new_col, 1, 0)
-
-    c_training ['other_' + col] = np.where(training_data[col] not in temp.keys(), 1, 0)
-    #c_test ['other_' + col] = np.where(test_data[col] not in temp.keys(), 1, 0)
-    # Collect all uncommon categories in other category    
-
-print(c_training.head())
-
-
 import network3 # this includes the neural network class
 from network3 import Network, ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer # This masks the classes from network3.py to be used without prefix
 
